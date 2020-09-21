ef831a7dac2206b30e18ef1f00667422c1036251
Theovady Moutty <theomoutty@gmail.com>
2020-09-21 13:35:03 +0200

    exo1 finsh

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
+    <!-- Ligne a modif avec v-for -->
+     <template v-for="(item, index) in items">
+        <router-link :key="index"   :to="item.path" >{{ item.title }}</router-link> |
+    </template>
+    <!-- -->
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/login">Login</router-link> 
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { path: '/', title: 'Home' },
+        { path: '/about', title: 'About' },
+        { path: '/login', title: 'Login' }
+      ]
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
