c845ba66c6c71078e680976b8c8aadc9ce4a0cbd
Theovady Moutty <theomoutty@gmail.com>
2020-09-21 10:49:02 +0200

    view login add compentents

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>    
+</template>
+
+<script>
+import FromLogin from '@/components/FromLogin.vue'
+
+export default {
+    components: {
+        FromLogin,
+    }
+}
+</script>
