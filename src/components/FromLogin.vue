ff4a37242c061c4653034fcb66024d5a4aba495c
Theovady Moutty <theomoutty@gmail.com>
2020-09-21 10:27:55 +0200

    first comit

---
+++ src/components/FromLogin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <form>
+            login: <br/>
+            Email : 
+            <input type='email' /> <br/>
+            password : <br/>
+            <input type='password'/> <br/>
+        </form>
+    </div>       
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+        email: '', 
+        password: '', 
+
+        }
+    }
+}
+</script>
